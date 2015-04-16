f288f993d65afc005b787a39d21bf8306135ca36
Everaldo Reis <contato@everaldoreis.com.br>
2015-04-16 17:20:23 -0300

    Change FILES_ROOT value from WEB_ROOT to WEB_PATH

--- config/fileman_constants.php
+++ config/fileman_constants.php
@@ -2,4 +2,4 @@
 define('FILEMAN_VERSION', '1.0');
 define('FILEMAN_FOLDER', 'fileman');
 define('FILEMAN_PATH', MODULES_PATH . FILEMAN_FOLDER . '/');
-defined('FILES_ROOT') || define('FILES_ROOT', '/' . WEB_FOLDER . '/assets/uploads');
+defined('FILES_ROOT') || define('FILES_ROOT', WEB_PATH . 'assets/uploads');
